769765327b79b959ac1fe8ed62b5e41d59d2a31c
UsamaBinHaleem524 <usama.binhaleem524@gmail.com>
2025-08-07 10:25:14 +0500

    Change the api url

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,11 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-require('colors')
-const authRoutes = require('./routes/authRoutes')
-const taskRoutes = require('./routes/taskRoutes')
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+require("colors");
+const authRoutes = require("./routes/authRoutes");
+const taskRoutes = require("./routes/taskRoutes");
 
-const connectDB = require('./config/db');
+const connectDB = require("./config/db");
 
 const app = express();
 
@@ -14,10 +14,13 @@ app.use(express.json());
 
 connectDB();
 
-app.use('/api/auth', authRoutes);
-app.use('/api/tasks', taskRoutes);
+app.get("/", (req, res) => {
+  res.send("Hello from Task Management App");
+});
+app.use("/api/auth", authRoutes);
+app.use("/api/tasks", taskRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log(`🚀 Server running on port ${PORT}`.cyan.bold);
-  });
+  console.log(`🚀 Server running on port ${PORT}`.cyan.bold);
+});
